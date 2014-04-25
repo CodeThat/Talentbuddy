37e8d1953e2d0654868dbefad67a7bab0bbbe917
Cameron Anglin <CodeThat@users.noreply.github.com>
2014-04-25 13:35:53 -0500

    Create SimpleSum.cpp
    Solution for simple sum.

---
+++ SimpleSum.cpp
@@ -0,0 +1,13 @@
+// Simple sum - Given two integer numbers A and B Your task is to write a function 
+// that prints to the standard output (stdout) their sum. Note that your function will 
+// receive the following arguments: 'a' which is the integer number A described above 'b' 
+// which is the integer number B described above.
+
+#include <iostream>
+
+using namespace std;
+
+void get_sum(int a, int b){
+   int c = a + b;
+   cout << c;
+}
