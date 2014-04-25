36d12e5d3510256b89d4ea006f137c1a18b7e5ad
Cameron Anglin <CodeThat@users.noreply.github.com>
2014-04-25 13:54:32 -0500

    Create count_occurrences.py
    Solution for count occurrences.

---
+++ count_occurrences.py
@@ -0,0 +1,17 @@
+'''
+Count occurences
+
+Given an array of sorted integers and a number K, your task is to print to the standard output (stdout) the number of occurences for K in the initial array.
+
+Expected complexity: lower than O(N)
+
+Example input:
+V = 1 2 3 3 3 3 4 4
+K = 3
+
+Example output:
+4
+'''
+
+def count_occurences(v, k):
+        print v.count(k)
